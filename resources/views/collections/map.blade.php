5b1230ebc4525c844c22f24c378267fd8a053d4e
Adrian Demleitner <adrian@thgie.ch>
2022-01-18 08:29:59 +0100

    added marker cluster plugin

--- resources/views/collections/map.blade.php
+++ resources/views/collections/map.blade.php
@@ -37,6 +37,8 @@
 </div>
 
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
+<link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.5.3/dist/MarkerCluster.css">
+<link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.5.3/dist/MarkerCluster.Default.css">
 <style>
 
     .leaflet-popup-content-wrapper {
@@ -50,6 +52,7 @@
 
 </style>
 <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+<script src="https://unpkg.com/leaflet.markercluster@1.5.3/dist/leaflet.markercluster.js"></script>
 
 <script>
 
@@ -57,11 +60,13 @@
         
         let center = [46.818188, 8.227512],
             map = L.map('map', {
-            center: center,
-            zoom: 8,
-            doubleClickZoom: false
-        });
+                center: center,
+                zoom: 8,
+                doubleClickZoom: false
+            }),
+            markers = L.markerClusterGroup();
 
+        map.addLayer(markers);
         map.doubleClickZoom.disable(); 
 
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -75,23 +80,25 @@
 
         @foreach ($collection->images as $image)
             @if ($image->location)
-            let image_{{ $image->id }} = new L.Marker([{{$image->location->latitude}}, {{$image->location->longitude}}]);
+                let image_{{ $image->id }} = new L.Marker([{{$image->location->latitude}}, {{$image->location->longitude}}]);
 
-            image_{{ $image->id }}
-                .addTo(map).bindTooltip('{{ $image->label }}')
-                .addEventListener('click', function(e) {
-                    window.location = '{{ route('images.show', [$image]) }}';
-                })
-                .bindPopup(
-                    `<img src="https://pia-iiif.dhlab.unibas.ch/{{$image->base_path}}/{{$image->signature}}.jp2/full/320,/0/default.jpg"/>`,
-                    {
-                        minWidth: 320,
-                        closeButton: false
-                    }
-                )
-                .on('mouseover', function (e) {
-                    this.openPopup();
-                });
+                image_{{ $image->id }}
+                    .bindTooltip('{{ $image->label }}')
+                    .addEventListener('click', function(e) {
+                        window.location = '{{ route('images.show', [$image]) }}';
+                    })
+                    .bindPopup(
+                        `<img src="https://pia-iiif.dhlab.unibas.ch/{{$image->base_path}}/{{$image->signature}}.jp2/full/320,/0/default.jpg"/>`,
+                        {
+                            minWidth: 320,
+                            closeButton: false
+                        }
+                    )
+                    .on('mouseover', function (e) {
+                        this.openPopup();
+                    });
+
+                markers.addLayer(image_{{ $image->id }});
             @endif
         @endforeach
 
