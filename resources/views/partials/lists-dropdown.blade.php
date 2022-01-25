f149cd4ef6eec0363b38bc0c735b97f209fa58ff
Adrian Demleitner <adrian@thgie.ch>
2022-01-25 11:08:34 +0100

    dropdown nav is now icons

--- resources/views/partials/lists-dropdown.blade.php
+++ resources/views/partials/lists-dropdown.blade.php
@@ -1,8 +1,14 @@
-<select name="lists" id="lists" class="text-xs px-2 bg-black text-white rounded-full" style="height: 26px;"
-    onchange="window.location = this.value">
-    <option value="">Lists…</option>
-    <option value="{{ route('collections.index') }}">Collections</option>
-    <option value="{{ route('keywords.index') }}">Keywords</option>
-    <option value="{{ route('people.index') }}">People</option>
-    <option value="{{ route('locations.index') }}">Locations</option>
-</select>
+<div class="inline-block mr-2" style="transform: translateY(8px)">
+    <a class="inline-block ml-2" href="{{ route('collections.index') }}" title="Collections">
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path></svg>
+    </a>
+    <a class="inline-block ml-2" href="{{ route('keywords.index') }}" title="Keywords">
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"></path></svg>
+    </a>
+    <a class="inline-block ml-2" href="{{ route('people.index') }}" title="People">
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+    </a>
+    <a class="inline-block ml-2" href="{{ route('locations.index') }}" title="Locations">
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+    </a>
+</div>
