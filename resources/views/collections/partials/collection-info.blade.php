dec07ca429afe543f3553b34a71a436180b4963c
Adrian Demleitner <adrian@thgie.ch>
2022-02-10 10:23:00 +0100

    fixed bug in collection header img

--- resources/views/collections/partials/collection-info.blade.php
+++ resources/views/collections/partials/collection-info.blade.php
@@ -14,7 +14,7 @@
     @php
     $image = $collection->images[rand(0, $collection->images()->count()-1)];
     @endphp
-    @if($image)
+    @if($collection->images->count() && $image)
     <div class="h-96 bg-center bg-cover mb-10"
         style="background-image: url('https://pia-iiif.dhlab.unibas.ch/{{$image->base_path != '' ? $image->base_path.'/' : ''}}{{$image->signature}}.jp2/full/960,/0/default.jpg')">
     </div>
