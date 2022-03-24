5681de0db47a4922ca39e74cc4bb310f31514233
Adrian Demleitner <adrian@thgie.ch>
2022-03-24 14:15:32 +0100

    added image details and similarity options

--- resources/views/images/similar.blade.php
+++ resources/views/images/similar.blade.php
@@ -9,6 +9,12 @@
                     <div class="flex gap-6 items-center">
                         <input type="text" name="query" placeholder="Search collection"
                             class="search py-2 px-6 w-2/3 border border-gray-700 rounded-full focus:outline-none text-lg z-10">
+
+                            <div class="flex gap-2 items-center print-hidden" x-data>
+                                <a class="text-xs mr-1 ml-2 underline text-white" href="{{ route('images.similar', [$image]) }}?category=localcolor">localcolor</a>
+                                <a class="text-xs mr-1 underline text-white" href="{{ route('images.similar', [$image]) }}?category=globalcolor">globalcolor</a>
+                                <a class="text-xs mr-1 underline text-white" href="{{ route('images.similar', [$image]) }}?category=localfeatures">localfeatures</a>
+                            </a>
                     </div>
                 </section>
             
@@ -21,6 +27,7 @@
         </div>
 
         <div class="fixed left-1/2 h-screen w-1/2 pr-36 bg-white overflow-y-auto">
+            @include('images.partials.details')
         </div>
     </div>
 
